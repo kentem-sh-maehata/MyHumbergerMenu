ad6fc0ad207978664649fafc6c55de071b66d539
kentem-sh-maehata <sh-maehata@kentem@.co.jp>
2024-05-16 10:53:17 +0900

    jsの動きまで

--- main.ts
+++ main.ts
@@ -2,18 +2,20 @@
   const open = document.getElementById('open')
   const close = document.getElementById('close')
   const menuArea = document.getElementById('menu-area')
-  console.log(open)
-  // open?.addEventListener('click',()=>{
 
   open?.addEventListener('click',()=>{
     console.log(menuArea)
     menuArea?.classList.add('show')
+    menuArea?.classList.remove('hidden')
+    
     close?.classList.remove('hidden')
     open?.classList.add('hidden')
   })
-
+  
   close?.addEventListener('click',()=>{
     menuArea?.classList.remove('show')
+    menuArea?.classList.add('hidden')
+
     close?.classList.add('hidden')
     open?.classList.remove('hidden')
   })
